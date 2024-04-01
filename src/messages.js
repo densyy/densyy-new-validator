d9e57f1ef3b3d89ceec642df6f8ff20eefb40401
Densyy <densyy@hotmail.com>
2024-04-01 18:56:04 -0300

    adiciona metodos min, max, minLength, maxLength em /src/messages

--- src/messages.js
+++ src/messages.js
@@ -9,11 +9,31 @@ function number (field) {
 }
 
 function string (field) {
-  return `O campo ${field} deve ser uma string`
+  return `O campo ${field} deve ser uma string.`
+}
+
+function min (field, min) {
+  return `O campo ${field} deve ser no mínimo ${min}.`
+}
+
+function max (field, max) {
+  return `O campo ${field} deve ser no máximo ${max}.`
+}
+
+function minLength (field, min) {
+  return `O campo ${field} deve ter no mínimo ${min} caracteres.`
+}
+
+function maxLength (field, max) {
+  return `O campo ${field} deve ter no máximo ${max} caracteres.`
 }
 
 module.exports = {
   required,
   number,
-  string
+  string,
+  min,
+  max,
+  minLength,
+  maxLength
 }
