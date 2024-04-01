58dd2ff7794c690663fd69a6430d0d3e42608835
Densyy <densyy@hotmail.com>
2024-04-01 18:52:19 -0300

    adiciona metodos string e number em /src/messages

--- src/messages.js
+++ src/messages.js
@@ -4,6 +4,16 @@ function required (field) {
   return `O campo ${field} é obrigatório.`
 }
 
+function number (field) {
+  return `O campo ${field} deve ser um número.`
+}
+
+function string (field) {
+  return `O campo ${field} deve ser uma string`
+}
+
 module.exports = {
-  required
+  required,
+  number,
+  string
 }
