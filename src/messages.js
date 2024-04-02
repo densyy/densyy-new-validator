cb80a490793ecca354ea6845f1adc26919aee62c
Densyy <densyy@hotmail.com>
2024-04-02 02:57:24 -0300

    refatora /src/messages

--- src/messages.js
+++ src/messages.js
@@ -1,69 +1,19 @@
 /* ---- Methods ---- */
 
-function required (field) {
-  return `O campo ${field} é obrigatório.`
+const Messages = {
+  required: (field) => `O campo ${field} é obrigatório.`,
+  number: (field) => `O campo ${field} deve ser um número.`,
+  string: (field) => `O campo ${field} deve ser uma string.`,
+  min: (field, min) => `O campo ${field} deve ser no mínimo ${min}.`,
+  max: (field, max) => `O campo ${field} deve ser no máximo ${max}.`,
+  minLength: (field, min) => `O campo ${field} deve ter no mínimo ${min} caracteres.`,
+  maxLength: (field, max) => `O campo ${field} deve ter no máximo ${max} caracteres.`,
+  date: (field) => `O campo ${field} deve ser uma data válida.`,
+  email: (field) => `O campo ${field} deve ser um email válido.`,
+  array: (field) => `O campo ${field} deve ser uma array.`,
+  hex: (field) => `O campo ${field} deve ser um valor em hexadecimal.`,
+  mongoid: (field) => `O campo ${field} deve ser um id válido.`,
+  size: (field, size) => `O campo ${field} deve conter ${size} elementos.`,
 }
 
-function number (field) {
-  return `O campo ${field} deve ser um número.`
-}
-
-function string (field) {
-  return `O campo ${field} deve ser uma string.`
-}
-
-function min (field, min) {
-  return `O campo ${field} deve ser no mínimo ${min}.`
-}
-
-function max (field, max) {
-  return `O campo ${field} deve ser no máximo ${max}.`
-}
-
-function minLength (field, min) {
-  return `O campo ${field} deve ter no mínimo ${min} caracteres.`
-}
-
-function maxLength (field, max) {
-  return `O campo ${field} deve ter no máximo ${max} caracteres.`
-}
-
-function date (field) {
-  return `O campo ${field} deve ser uma data válida.`
-}
-
-function email (field) {
-  return `O campo ${field} deve ser um email válido.`
-}
-
-function array (field) {
-  return `O campo ${field} deve ser uma array.`
-}
-
-function hex (field) {
-  return `O campo ${field} deve ser um valor em hexadecimal.`
-}
-
-function mongoid (field) {
-  return `O campo ${field} deve ser um id válido.`
-}
-
-function size (field, size) {
-  return `O campo ${field} deve conter ${size} elementos.`
-}
-
-module.exports = {
-  required,
-  number,
-  string,
-  min,
-  max,
-  minLength,
-  maxLength,
-  date,
-  email,
-  array,
-  hex,
-  mongoid,
-  size
-}
+module.exports = Messages
